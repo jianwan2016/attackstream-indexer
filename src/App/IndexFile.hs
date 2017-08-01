0802155d2e1a6337e44711ada51649b71048140e
Jian Wan <jwan@arbor.net>
2017-08-01 15:14:13 +1000

    MAYH-5930 comma separated word64 in index file

--- src/App/IndexFile.hs
+++ src/App/IndexFile.hs
@@ -1,21 +1,21 @@
 {-# LANGUAGE RecordWildCards #-}
 module App.IndexFile where
 
-import Data.ByteString.Char8
-import Data.Word
-import HaskellWorks.Data.Xml
-import HaskellWorks.Data.RankSelect.Poppy512S
-import HaskellWorks.Data.BalancedParens.RangeMinMax
-import HaskellWorks.Data.BalancedParens.Simple
-import HaskellWorks.Data.Bits.BitShown
-import HaskellWorks.Data.FromByteString
-import qualified Data.Vector.Storable as DVS
-import qualified Data.ByteString.Base64 as Base64
-import qualified Data.ByteString.Lazy.Internal as BLI
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Conversion as C
-import qualified Data.ByteString.Internal as BSI (w2c)
-import Text.XML.Light
+import qualified Data.ByteString                              as BS
+import qualified Data.ByteString.Base64                       as Base64
+import           Data.ByteString.Char8
+import qualified Data.ByteString.Conversion                   as C
+import qualified Data.ByteString.Internal                     as BSI (w2c)
+import qualified Data.ByteString.Lazy.Internal                as BLI
+import qualified Data.Vector.Storable                         as DVS
+import           Data.Word
+import           HaskellWorks.Data.BalancedParens.RangeMinMax
+import           HaskellWorks.Data.BalancedParens.Simple
+import           HaskellWorks.Data.Bits.BitShown
+import           HaskellWorks.Data.FromByteString
+import           HaskellWorks.Data.RankSelect.Poppy512S
+import           HaskellWorks.Data.Xml
+import           Text.XML.Light
 
 --TODO duplicates code in unifier - move to common place
 type Cursor = XmlCursor BS.ByteString Poppy512S (RangeMinMax Poppy512S)
@@ -37,9 +37,9 @@ xmlIndexVersion :: String
 xmlIndexVersion = "1.0"
 
 data XmlIndex = XmlIndex
-  { xiVersion          :: String
-  , xiInterests        :: String
-  , xiBalancedParens   :: String
+  { xiVersion        :: String
+  , xiInterests      :: String
+  , xiBalancedParens :: String
   }
 
 instance Node XmlIndex where
@@ -51,7 +51,9 @@ instance Node XmlIndex where
       ]
 
 word64ToStrictBS :: Word64 -> ByteString
-word64ToStrictBS w = BLI.foldlChunks append BS.empty (C.toByteString w)
+word64ToStrictBS w = do
+  let bs = BLI.foldlChunks append BS.empty (C.toByteString w)
+  bs `append` ","
 
 vectorToStrictBS :: DVS.Vector Word64 -> BS.ByteString
 vectorToStrictBS = DVS.foldl' f BS.empty
